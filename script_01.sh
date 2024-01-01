453d9ee7c12af3e12b27c943299673b0f85c8212
Giovanni Zuccolo <80920113+giovannizuccolo@users.noreply.github.com>
2024-01-01 16:22:43 -0300

    Add files via upload

---
+++ script_01.sh
@@ -0,0 +1,28 @@
+!# /bin/bash
+
+echo -e "Entrando \t no diretório raiz.\n"
+cd /
+
+echo -e "Em qual diretório estou?.\n"
+pwd
+
+echo -e "Listando arquivos do diretório.\n"
+ls
+
+
+
+
+echo "Meu nome é Giovanni"
+echo "tudo bem?"
+echo "oi"
+
+
+echo "
+
+
+oii
+tudo bem?
+meu nome é Giovanni.
+
+
+"
